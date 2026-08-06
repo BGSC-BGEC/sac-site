3b92c23b5c62495c4d618d322be4c30c47b8abc3
Dhruvin Davada <d.h.dhruvin@gmail.com>
2026-08-06 10:39:38 +0530

    ma ka bhosda

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "@fontsource/inter/400.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 import "./index.css";
